39bb9649b5a7ece872ef9485e3fa4f13c0c6bf95
Gusvioli <gustavovieiradeoliveira@gmail.com>
2022-06-10 23:01:30 -0300

    Exercicio de React 10.1 completo

---
+++ exercicios/front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios/textos/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+const arr = ['Estudar React', 'Estudar Node', 'Estudar React Native'];
+
+const Task = (value) => {
+  return (
+    <ul>{ arr.map((task) => ( <li>{ task }</li>))}</ul>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div>{ Task() }</div>
+    </div>
+  );
+}
+
+export default App;
